da4999fc1a780c368cfb0a77102671f43744bdc8
Titus Denis <deetitos@gmail.com>
2024-03-28 01:14:52 +0100

    9-binary_tree_height.c

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -8,10 +8,12 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
+	size_t lh, rh;
+
 	if (tree == NULL)
 		return (0);
-	size_t lh = tree->left ? 1 + binary_tree_height(tree->left) : 0;
-	size_t rh = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+	lh = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+	rh = tree->right ? 1 + binary_tree_height(tree->right) : 0;
 
 	return ((lh > rh) ? (lh) : (rh));
 }
